e1099b3dad8df8b4d03664e54f84b2c71a28ffb5
isaka <c0b220090b@edu.teu.ac.jp>
2023-09-26 16:59:29 +0900

    演習問題(背景)

--- pygame_image.py
+++ pygame_image.py
@@ -11,17 +11,19 @@ def main():
     rot_kk_img = pg.transform.rotozoom(kk_img,-10,1.0)
     lst = [kk_img, rot_kk_img]
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    back_bg_img = pg.transform.flip(bg_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = tmr%1500
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(back_bg_img, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(lst[tmr%2], [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
