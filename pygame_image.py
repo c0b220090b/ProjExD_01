e59c2ade9041a6a1aee9d12c2fd278ef54d01514
isaka <c0b220090b@edu.teu.ac.jp>
2023-09-26 15:33:30 +0900

    練習問題３問目

--- pygame_image.py
+++ pygame_image.py
@@ -7,7 +7,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img,True, False)
+    rot_kk_img = pg.transform.flip(kk_img,True, False)
+    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
+    lst = [kk_img, rot_kk_img]
     bg_img = pg.image.load("fig/pg_bg.jpg")
     tmr = 0
     while True:
